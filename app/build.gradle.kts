0a4c0e05428c338408db6587f78c0838e2f6cb6b
jkhin_cenco <joel.colmenares@externos-cl.cencosud.com>
2024-04-23 22:08:08 -0500

    second commit - fixed settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,6 @@ android {
 }
 
 dependencies {
-    // implementation(project(":nebula"))
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -67,4 +65,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // NEBULA Lib
+    implementation(libs.nebula.sample)
 }
