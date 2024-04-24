9ca6c3166bbc85bd596c306252e5852ca9a94e91
jkhin_cenco <joel.colmenares@externos-cl.cencosud.com>
2024-04-23 21:12:50 -0500

    lib sample

---
+++ app/src/main/java/jk/labs/sample/sample/compose/lib/MainActivity.kt
@@ -0,0 +1,26 @@
+package jk.labs.sample.sample.compose.lib
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import jk.labs.sample.sample.compose.lib.ui.theme.SampleComposeLibTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleComposeLibTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // NText(text = "Hello Joe!")
+                }
+            }
+        }
+    }
+}
