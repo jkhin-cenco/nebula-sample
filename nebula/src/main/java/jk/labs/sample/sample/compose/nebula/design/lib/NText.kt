9c7ece269b7aa4326b1c609f2b9be22cbfedbf82
jkhin_cenco <joel.colmenares@externos-cl.cencosud.com>
2024-04-24 17:03:05 -0500

    ran tasks, checked version, status: stable

--- nebula/src/main/java/jk/labs/sample/sample/compose/nebula/design/lib/NText.kt
+++ nebula/src/main/java/jk/labs/sample/sample/compose/nebula/design/lib/NText.kt
@@ -3,7 +3,6 @@ package jk.labs.sample.sample.compose.nebula.design.lib
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
-
 @Composable
 fun NText(text: String) {
     Text(text = text)
